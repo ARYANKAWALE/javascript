59bc59f7dd63a110f3bf19c47a4278622a1bd9af
Aryan Kawale <aryankawale163@gmail.com>
2025-09-14 10:36:39 +0000

    Add initial variable declarations and logging example

---
+++ basics/variable.js
@@ -0,0 +1,12 @@
+const first_name = "aryan";
+let middle_name = "Devidas";
+var  last_name = "kawale";
+address = "Nagpur";
+
+console.table([first_name,middle_name,last_name,address])
+
+// first_name = "Sanket";
+// middle_name = "kawale";
+// last_name = "Devidas";
+
+// console.log(first_name);
