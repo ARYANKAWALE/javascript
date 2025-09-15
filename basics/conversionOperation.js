d8fc2d5832aab2b78815f5a698b8bef9cadbf395
Aryan Kawale <aryankawale163@gmail.com>
2025-09-15 15:58:28 +0000

    learn js datatype conversion

---
+++ basics/conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "33";
+
+// console.log(typeof score);
+
+
+// console.log(typeof(score));
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+//  "33" => 33
+//  "33fs" => NaN
+//  true => 1; false => false
+
+
+let isLoggedIn = "aryan"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+//  "" => false
+//  "Aryan" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
