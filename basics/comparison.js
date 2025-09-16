d4a7509ab0d96db088293db3db6d8329dfdb5835
Aryan Kawale <aryankawale163@gmail.com>
2025-09-16 11:40:45 +0000

    learned about comparisonsin js

---
+++ basics/comparison.js
@@ -0,0 +1,10 @@
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true because javascript assume that null value is 0
+
+// console.log(undefined > 0);  //false
+// console.log(undefined == 0);  //false
+// console.log(undefined >= 0);  //false
+
+//  ===
+console.log("2" === 2);  // (===)<-----checks strictly
--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -2,7 +2,6 @@ let score = "33";
 
 // console.log(typeof score);
 
-
 // console.log(typeof(score));
 let valueInNumber = Number(score);
 // console.log(typeof valueInNumber);
@@ -11,8 +10,7 @@ let valueInNumber = Number(score);
 //  "33fs" => NaN
 //  true => 1; false => false
 
-
-let isLoggedIn = "aryan"
+let isLoggedIn = "aryan";
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 // console.log(booleanIsLoggedIn)
@@ -21,9 +19,29 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 //  "" => false
 //  "Aryan" => true
 
-let someNumber = 33
+let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
+// console.log(stringNumber);
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+// *******************Operations*****************//
+
+value = 3;
+let negvalue = -value;
+// console.log(negvalue);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "7");
+// console.log("7" + 1 + 2);
+
+// console.log(((3 + 4) * 5) % 4);
+
+
+let num1,num2,num3;
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
