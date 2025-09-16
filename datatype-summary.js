04822936ad215c432012c9cf3b63caec092d79dc
Aryan Kawale <aryankawale163@gmail.com>
2025-09-16 12:33:42 +0000

    learned datatype

---
+++ datatype-summary.js
@@ -0,0 +1,31 @@
+// primitive datatype
+
+// 7 types : String,boolean,BigInt,Number,undefined,symbol,null
+
+// const bigNumber = 2435635423453245324355n
+const isLoggedIn = false
+const outsideTemp = null
+let userEmaill;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+
+// Reference datatype
+// Array,Object,Functions
+
+const heros = ["shaktiman","nagraj","doga"];
+let myObj = {
+    name:"aryan",
+    age:18,
+}
+
+const myFunction =  ()=>{
+console.log("Hello Reader");
+}
+
+console.log(typeof myFunction);
+
+// https://262.ecma-international.org/5.1/#sec-11..4.3
