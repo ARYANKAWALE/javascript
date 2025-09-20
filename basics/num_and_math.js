6055f5e7810326c1858d8d5997f814cc2ac94d7e
Aryan Kawale <aryankawale163@gmail.com>
2025-09-20 17:22:39 +0000

    learned about the math and nums

---
+++ basics/num_and_math.js
@@ -0,0 +1,28 @@
+let score = 100;
+
+// console.log(score)
+
+let cost = new Number(1010)
+// console.log(cost)
+
+// console.log(cost.toString().length)
+
+// console.log(cost.toFixed(10))
+
+let num = 25.2523;
+// console.log(num.toPrecision(3))
+
+let count = 10000000;
+// console.log(count.toLocaleString('en-IN'))
+
+// **************************MATH**********************************
+
+// console.log(Math)
+// console.log(Math.abs(-4)) //returns absolute value make - value to +.keeps + to +
+// console.log(Math.abs(4)) //this absolute will return + value as it is
+// console.log(Math.max(4,2,6,8,5,7)) //this will find the maximum value
+// console.log(Math.min(4,5,6,3,2)) //this will find the minimum value
+// console.log(Math.ceil(4.3)) //this will always return the top most value as integer
+// console.log(Math.floor(3.4)) //this will always return the lowest value as integer
+// console.log(Math.round(4.5)) //round will return the closest value
+console.log(Math.random())
