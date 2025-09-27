6d801332178401b9a64c58cfe66aabac855d4df0
Aryan Kawale <aryankawale163@gmail.com>
2025-09-27 13:38:28 +0000

    learned creating objects, merging them, working with arrays of objects, and using built-in object methods.

--- 02_basics/object_2.js
+++ 02_basics/object_2.js
@@ -1,9 +1,59 @@
 // const tinderUser = new Object()
 
-const tinderUser = {}
+const tinderUser = {};
 
-tinderUser.id = "123abc"
-tinderUser.name = "mayank"
-tinderUser.isloggedIn = false
+tinderUser.id = "123abc";
+tinderUser.name = "mayank";
+tinderUser.isloggedIn = true;
 
 // console.log(tinderUser)
+
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "a", 4: "b" };
+const obj3 = { 5: "a", 6: "b" };
+
+//  const obj4 = (obj1,obj2);
+
+// const obj4 = Object.assign({},obj1,obj2,obj3)  //{}--->target
+
+const obj4 = { ...obj1, ...obj2, ...obj3 };
+// console.log(obj4);
+
+const users = [
+  {
+    email: "aryan@gmail.com",
+    name: "jdgg",
+  },
+  {
+    email: "wb@gmail.com",
+    name: "jdgg",
+  },
+  {
+    email: "wctan@gmail.com",
+    name: "jdgg",
+  },
+  {
+    email: "btwc@gmail.com",
+    name: "jdgg",
+  },
+  {
+    email: "wbc@gmail.com",
+    name: "jdgg",
+  },
+  {
+    email: "wctb@gmail.com",
+    name: "jdgg",
+  },
+];
+
+// console.log(users[1])
+
+// console.log(tinderUser)
+
+// console.log(Object.keys(tinderUser))
+
+// console.log(Object.values(tinderUser))
+
+// console.log(Object.entries(tinderUser))
+
+console.log(tinderUser.hasOwnProperty('isloggedIn'))
