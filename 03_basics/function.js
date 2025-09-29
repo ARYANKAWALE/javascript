06f87e33ae9039fb544dfeb8db61fe20b37b6b46
Aryan Kawale <aryankawale163@gmail.com>
2025-09-29 14:52:37 +0000

    functions

--- 03_basics/function.js
+++ 03_basics/function.js
@@ -20,24 +20,52 @@ const sayMyName = () => {
 // addTwoNumber(2,"4");
 
 const addNumber = (number1, number2) => {
-//   let result = number1 + number2;
-//   return result
-return number1  + number2;
-}
-const result = addNumber(2,4)
+  //   let result = number1 + number2;
+  //   return result
+  return number1 + number2;
+};
+const result = addNumber(2, 4);
 // console.log(result)
-
 
 // const loginUser = (username) => {
 //     return `${username} just logged in`
 // }
 const loginUser = (username = "aryan") => {
-    if(username === undefined){
-        console.log("please enter the name")
-        return
-    }
-    return `${username} just logged in`
-}
+  if (username === undefined) {
+    console.log("please enter the name");
+    return;
+  }
+  return `${username} just logged in`;
+};
 
 // console.log(loginUser("Aryan"));
-console.log(loginUser());  // if not passed anything it will return undefined
+// console.log(loginUser());  // if not passed anything it will return undefined
+
+const calculatePrice = (...num1) => {
+  return num1;
+};
+// console.log(calculatePrice(200,888,999))
+
+const user = {
+  username: "Aryan",
+  price: 199,
+};
+
+function handleObject(anyobject) {
+  console.log(
+    `the name is ${anyobject.username} and prices is ${anyobject.price}`
+  );
+}
+// handleObject(user);
+// handleObject({
+//   username: "Vrushali",
+//   price: 30,
+// });
+
+const myArray = [100, 300, 200, 500];
+
+const returnSecondValue = (getArray) => {
+  return myArray[1];
+};
+// console.log(returnSecondValue(myArray))
+console.log(returnSecondValue([200,400,100,600]))
