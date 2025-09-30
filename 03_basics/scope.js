0a020d489858211ab5d3ad41fbe61240a5216fe9
Aryan Kawale <aryankawale163@gmail.com>
2025-09-30 14:20:10 +0000

    learned scopes

---
+++ 03_basics/scope.js
@@ -0,0 +1,11 @@
+
+if (true){
+    let a = 10;
+    const b = 30;
+    var c = 20;  //var is a global scope variable so we don't use this
+    console.log("Value:",a)
+
+}
+// console.log(a);
+// console.log(b);
+console.log(c);
