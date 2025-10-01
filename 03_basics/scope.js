ee789afe79e7080df7d5e410b64cb906941f74ba
Aryan Kawale <aryankawale163@gmail.com>
2025-10-01 13:58:28 +0000

    hoisting and scope level

--- 03_basics/scope.js
+++ 03_basics/scope.js
@@ -1,11 +1,44 @@
-
-if (true){
-    let a = 10;
-    const b = 30;
-    var c = 20;  //var is a global scope variable so we don't use this
-    console.log("Value:",a)
-
+if (true) {
+  let a = 10;
+  const b = 30;
+  var c = 20; //var is a global scope variable so we don't use this
+  //   console.log("Value:", a);
 }
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+// function one() {
+//   const username = "Aryan";
+//   function two() {
+//     const website = "youtube";
+//     // console.log(username);
+//   }
+//   //   console.log(website);
+
+//   two();
+// }
+// one();
+
+if (true) {
+  const user = "Aryan";
+  if (user === "Aryan") {
+    const website = "Weather";
+    // console.log(user + website);
+  }
+  // console.log(website)
+}
+// console.log(user)
+
+
+// ***********************intresting****************************
+
+function addOne(num){
+    return num +1;
+}
+addOne()
+
+const number = function addTwo(num){
+    return num + 2
+}
+addTwo()
